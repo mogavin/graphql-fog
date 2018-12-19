eb847766221ee6955b99a5419e33bd2128976e9f
mogavin <motagavino@gmail.com>
2018-12-19 10:55:51 -0200

    test (queries): Adiciona testes de queries para o type Jogo
    #NoIssue

---
+++ src/graphql/testes/jogo.queries.test.js
@@ -0,0 +1,128 @@
+const { assert } = require('chai'),
+{ graphql, buildSchema } = require('graphql'),
+{ makeExecutableSchema } = require('graphql-tools'),
+criarJogoResolver = require('../resolvers/Jogo'),
+criarJogoRepositorio = require('../../repositorio/jogo'),
+type = require('../tipos/Jogo');
+
+describe('Jogo Queries', () => {
+  const JOGOS = [
+    {
+      id: 1,
+      serie: 'Sonic The Hedgehog',
+      titulo: 'Sonic The Hedgehog 2',
+      genero: 'Plataforma',
+    },
+    {
+      id: 2,
+      serie: 'Mario', 
+      titulo: 'Super Mario World',
+      genero: 'Plataforma',
+    },
+    {
+      id: 3,
+      serie: 'Final Fantasy',
+      titulo: 'Final Fantasy VII',
+      genero: 'RPG',
+    },
+  ],
+  db = [],
+  repositorio = criarJogoRepositorio(db),
+  resolver = criarJogoResolver(repositorio),
+  schema = makeExecutableSchema({
+    typeDefs: [type],
+    resolvers: [resolver],
+  }),
+  limparDb = () => db.splice(0, db.length),
+  popularDb = () => db.push(...JOGOS);
+
+  beforeAll(() => {
+    limparDb();
+    popularDb();
+  });
+
+
+  it('deve buscar todos os jogos', async () => {
+    const query = `
+      {
+        jogos {
+          serie
+        }  
+      }
+    `;
+    const actual = await graphql(schema, query),
+    expected = { 
+      data: { 
+        jogos: [
+          {serie: 'Sonic The Hedgehog'},
+          {serie: 'Mario'},
+          {serie: 'Final Fantasy'},
+        ],
+      } 
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('deve buscar jogos pelo id', async () => {
+    const query = `
+      {
+        jogo(id: 1) {
+          serie
+        }  
+      }
+    `;
+    const actual = await graphql(schema, query),
+    expected = { 
+      data: { 
+        jogo: { 
+          serie: 'Sonic The Hedgehog',
+        } 
+      } 
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('deve criar um novo jogo', async () => {
+    const mutation = `
+      mutation {
+        jogoCreate(input: {serie: "Resident Evil", titulo: "Resident Evil 3: Nemesis", genero: Acao}) {
+          serie
+        }
+      }
+    `;
+    const actual = await graphql(schema, mutation),
+    expected = { 
+      data: { 
+        jogoCreate: { 
+          serie: 'Resident Evil',
+        } 
+      } 
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('deve atualizar dados de um jogo existente', async () => {
+    const mutation = `
+      mutation {
+        jogoUpdate(id: 3, input: { titulo: "Final Fantasy X" }) {
+          id
+          titulo
+        }
+      }
+    `;
+    const actual = await graphql(schema, mutation),
+    expected = { 
+      data: { 
+        jogoUpdate: { 
+          id: '3',
+          titulo: 'Final Fantasy X',
+        } 
+      }
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+});
--- src/graphql/tipos/Jogo/index.js
+++ src/graphql/tipos/Jogo/index.js
@@ -21,4 +21,5 @@ type Jogo {
 enum Genero {
   Plataforma
   RPG
+  Acao
 }`;
